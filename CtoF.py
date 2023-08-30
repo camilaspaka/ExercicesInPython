2c9c484b07daa53008012079b4c62cf988269f86
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-08-30 15:08:37 -0300

    Update CtoF.py
    doing the inverse now

--- CtoF.py
+++ CtoF.py
@@ -1,8 +1,8 @@
 # Python Program to convert temperature in celsius to fahrenheit
 
 # change this value for a different result
-celsius = 23
+fahrenheit = 98
 
 # calculate fahrenheit
-fahrenheit = (celsius * 1.8) + 32
+celsius = (fahrenheit - 32) / 1.8
 print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
