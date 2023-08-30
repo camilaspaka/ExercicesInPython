77674426e503c585e4116dc61c44f82b99795bc2
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-08-30 15:10:20 -0300

    Update PositiveNegativeOr0.py
    negative works just fine

--- PositiveNegativeOr0.py
+++ PositiveNegativeOr0.py
@@ -1,7 +1,8 @@
 num = float(input("Enter a number: "))
-if num > 0:
-   print("Positive number")
-elif num == 0:
-   print("Zero")
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
 else:
    print("Negative number")
