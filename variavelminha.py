6752aaa66c1e5192c0c9bb743c0b8e0dc40cc054
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-08-31 15:03:59 -0300

    Create variavelminha.py
    teste da facul

---
+++ variavelminha.py
@@ -0,0 +1,23 @@
+# criando as variáveis:
+
+nome = ‘Camila Pinho Spakauskas’
+
+idade = 28
+
+endereco = ‘Rua Bom Pastor,1674 ’
+
+altura = 1.60
+
+peso = 114
+
+# mostrando:
+
+print(‘Nome:’, nome)
+
+print(‘Idade:’, idade,‘anos’)
+
+print(‘Endereço:’, endereco)
+
+print(‘Altura:’, altura)
+
+print(‘Peso:’, peso,‘kgs’)
