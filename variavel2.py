da632ce651b4738434686d201fff656683835187
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-01 15:11:32 -0300

    Create variavel2.py
    novo exemplo

---
+++ variavel2.py
@@ -0,0 +1,6 @@
+responsavel=input("Digite o nome do responsavel: ")
+funcionario=input("Digite o nome do funcionario: ")
+evento=input("Digite o nome do evento: ")
+valor=float(input("Digite o valor que será ressarcido: "))
+
+print("Declaro para o senhor(a)" + responsavel + ", que o/a senhor(a) " + funcionario + " esteve presente no evento " + evento + "e gastou o valor de R$ " + str(valor) + "com a entrada.") 
