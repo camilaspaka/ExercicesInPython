5c0293fdea0e5c0fb19c805285ef46975367df1c
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-01 12:20:18 -0300

    variaveis
    alguns testes e exercicios feitos

---
+++ variable.py
@@ -0,0 +1,8 @@
+nome="Camila Spakauskas"
+empresa='Foundever'
+qtde_funcionarios=4000
+mediaSalario=1555
+#ele é apenas leitura então nada vai acontecer visualmente
+print(nome + " trabalha na empresa " + empresa)
+print("Possui: ", qtde_funcionarios, "funcionarios.")
+print("A média salarial é de: " + str(mediaSalario))
--- variavelminha.py
+++ variavelminha.py
@@ -1,23 +1,18 @@
 # criando as variáveis:
-
-nome = ‘Camila Pinho Spakauskas’
-
+nome = "Camila Pinho Spakauskas"
 idade = 28
-
-endereco = ‘Rua Bom Pastor,1674 ’
-
+endereco = "Rua Bom Pastor,1674"
 altura = 1.60
-
-peso = 114
+peso = 114.40
 
 # mostrando:
 
-print(‘Nome:’, nome)
+print("Nome: ", + nome)
 
-print(‘Idade:’, idade,‘anos’)
+print("Idade: ", + idade, "anos")
 
-print(‘Endereço:’, endereco)
+print("Endereço: ", + endereco)
 
-print(‘Altura:’, altura)
+print("Altura: ", + str(altura))
 
-print(‘Peso:’, peso,‘kgs’)
+print("Peso: ", + str(peso), "kgs")
