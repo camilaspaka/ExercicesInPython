8dda8880abd839022e83f8a54496530bf610325b
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-04 15:59:30 -0300

    Update whattodo.py
    agora funcionou, não estava em caps lock por isso o erro

--- whattodo.py
+++ whattodo.py
@@ -3,7 +3,9 @@ idade=int(input("Digite a idade: "))
 doenca_infectocontagiosa=input("Suspeita de doença infecto-contagiosa?").upper()
 if idade>=65:
     print("O paciente " + nome + " possui atendimento prioritário!")
-elif doenca_infectocontagiosa=="Sim":
+elif doenca_infectocontagiosa=="SIM":
     print("O paciente " + nome + " deve ser direcionado a sala de espera reservada.")
 else:
     print("O paciente " + nome + " não possui atendimento prioritário e pode aguardar")
+
+    #não sei porque mas o elif não ta funcionando...chateação 
