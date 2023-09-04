31e8220116ece52daaa5514ad3fac58b56abf165
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-04 13:50:01 -0300

    Create if.py
    exercicio em if

---
+++ if.py
@@ -0,0 +1,6 @@
+nota=8
+if nota>=6 :
+    print("VocÊ está de...")
+    print("PARABENS!!!")
+    print("Este texto ainda sera exibido se a nota for maior que seis")
+    print("A exibição independe da condição if nesse exemplo")
