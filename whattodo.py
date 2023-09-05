66c2728f89dc3d72e419c775c2f434f89d4ed06e
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-05 11:28:12 -0300

    Update whattodo.py
    teste com and, funcional

--- whattodo.py
+++ whattodo.py
@@ -1,12 +1,14 @@
 nome=input("Digite o nome: ")
 idade=int(input("Digite a idade: "))
 doenca_infectocontagiosa=input("Suspeita de doença infecto-contagiosa?").upper()
-if idade>=65:
-    print("O paciente " + nome + " possui atendimento prioritário!")
-elif doenca_infectocontagiosa=="SIM":
-    print("O paciente " + nome + " deve ser direcionado a sala de espera reservada.")
-else:
-    print("O paciente " + nome + " não possui atendimento prioritário e pode aguardar")
-
     #não sei porque mas o elif não ta funcionando...chateação 
     #descobri o porque, resolvido ja
+    #código será alterado para preencher todas as possibilidades
+if idade>=65 and doenca_infectocontagiosa=="SIM":
+    print("O paciente será direcionado para a Sala Amarela - Com Prioridade")
+elif idade <65 and doenca_infectocontagiosa=="SIM":
+    print("O paciente será direcionado para a Sala Amarela - Sem Prioridade")
+elif idade>=65 and doenca_infectocontagiosa=="NÃO":
+    print("O paciente será direcionado para a Sala Branca - Com prioridade")
+else:
+    print("Responda a suspeita da doença infectocontagiosa com SIM ou NÃO")
