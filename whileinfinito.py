013940a820a887b15976e103d4013b83c6bca0bc
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-05 16:21:51 -0300

    Update whileinfinito.py

--- whileinfinito.py
+++ whileinfinito.py
@@ -1,5 +1,6 @@
 numero=int=(input("Digite um numero: "))
-while numero<100:
+while numero=<100:
     print(" " + str(numero))
     numero=numero+1
 print("Laço encerrado...")
+#o que está dando errado? não sei sinceramente logo mais do uma olhada
