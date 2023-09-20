ec81bf6d066a0c5423f743d1f18e6f805bb88e33
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-20 18:39:04 -0300

    funções
    mais exercicos sobre funções

---
+++ preencherinventario.py
@@ -0,0 +1,16 @@
+def preencherInventario(lista):
+      resp="S"
+  while resp == "S":
+    equipamento=[input("Equipamento: "),
+              float(input("Valor: ")),
+              int(input("Número Serial: ")),
+              input("Departamento: ")]
+    lista.append(equipamento)
+    resp = input("Digite "S" para continuar: ").upper()
+
+def exibirInventario(lista):
+  for elemento in lista:
+    print("Nome.........: ", elemento[0])
+    print("Valor........: ", elemento[1])
+    print("Serial.......: ", elemento[2])
+    print("Departamento.: ", elemento[3])
