1b5582ea05d1e67b6640ff9138bf62dfe5a582e7
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-21 17:04:50 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -3,3 +3,7 @@ usuarios={
     "Chaves":["Chaves Silva","17/06/2017","Recep_01"],
     "Quico":["Enrico Flores","03/06/2017","Raiox_02"]
     }
+
+#adicionando itens no dicionario
+
+usuarios["Florinda"]=["Florinda Flores", "26/07/23", "Recep_01"]
