13b44f0fffa16d77113e6c98898e34fcaebfa89a
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-24 13:01:49 -0300

    Update manageusers.py

--- manageusers.py
+++ manageusers.py
@@ -7,7 +7,7 @@ opcao=input("O que deseja realizar?" +
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
     if opcao=="I":
 chave=input("Digite o login: ").upper()
-usuarios[chave]=[input("Digite o nome: ").upper(),
+usuarios[input("Digite o login: ").upper()]=[input("Digite o nome: ").upper(),
                  input("Digite a última data de acesso: "),
                  input("Qual a última estação acessada: ").upper()]
     opcao = input("O que deseja realizar?" +
