16fd48fd30b873c888970186495fe15a224176e6
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-24 14:04:37 -0300

    Update manageusers.py

--- manageusers.py
+++ manageusers.py
@@ -16,10 +16,17 @@ usuarios[input("Digite o login: ").upper()]=[input("Digite o nome: ").upper(),
                   "<E> - Para Excluir um usuário" +
                   "<L> - Para Listar um usuário: ").upper()
 
-from funcoes2 import *
+from dicionario import *
 usuarios={}
+
 opcao=perguntar()
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
     if opcao=="I":
         inserir(usuarios)
+    if opcao=="P":
+        pesquisar(usuarios,input("Qual login deseja pesquisar? "))
+    if opcao == "E":
+        excluir(usuarios,input("Qual login deseja excluir? "))
+    if opcao == "L":
+        listar(usuarios)
     opcao = perguntar()
