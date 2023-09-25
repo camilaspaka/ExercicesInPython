8718e704a3490129d9b02b0c21e9ad1754c90b81
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-25 17:07:21 -0300

    Update tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -4,6 +4,9 @@ while resp=="S":
     ips[(input("Digite os dois primeiros octetos: "),
        input("Digite os dois últimos octetos: "))]=input("Nome da máquina: ")
     resp=input("Digite <S> para continuar: ").upper()
-print("Exibindo ip´s: ")
-for ip in ips.keys():
-    print(ip[0]+"."+ip[1])
+print("Exibindo máquinas com o mesmo endereço: ")
+pesquisa=input("Digite os dois últimos octetos: ")
+for ip,nome in ips.items():
+    print("Máquinas no mesmo endereço (redes diferentes)")
+    if(ip[1]==pesquisa):
+        print(nome)
