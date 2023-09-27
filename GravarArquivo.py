7fc1b47ac54dbf24811d8ad1484648a585345cf2
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-27 18:24:22 -0300

    teste2
    teste sobre teste

--- GravarArquivo.py
+++ GravarArquivo.py
@@ -1,5 +1,5 @@
 with open("teste.txt", "w") as arquivo:
     arquivo.write("Nunca foi tão fácil criar um arquivo.")
 
-with open("teste.txt", "w") as arquivo:
+with open("teste.txt", "a") as arquivo:
     arquivo.write("Continuação do texto.")
