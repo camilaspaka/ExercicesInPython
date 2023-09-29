3d1cdc60a834fe34a25d8caf04ec0a5326a070e0
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-09-29 16:14:43 -0300

    Update Inventario.py

--- Inventario.py
+++ Inventario.py
@@ -55,3 +55,19 @@ while opcao>0 and opcao<4:
         for linha in resultado:
             print(linha[2:-1])
     opcao = chamarMenu()
+
+    #testar depois 
+
+    from Funcoes.Funcoes_Arquivos import *
+inventario={}
+opcao=chamarMenu()
+while opcao>0 and opcao<4:
+    if opcao==1:
+        registrar(inventario)
+    elif opcao==2:
+        persistir(inventario)
+    elif opcao==3:
+        resultado = exibir()
+        for linha in resultado:
+            print(linha[linha.find(";")+1:-1])
+    opcao = chamarMenu()
