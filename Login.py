c0ae549f0611dd3da8c6918697534b6e3f5478d1
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-11 15:26:39 -0300

    teste
    teste

--- Login.py
+++ Login.py
@@ -1,7 +1,6 @@
 import getpass
 
-usuario = input("
-Digite o usuário: ").upper()
+usuario = input("Digite o usuário: ").upper()
 senha = getpass.getpass("Digite a senha: ")
 
 if usuario == "BITMED" and senha == "FiAp1222":
