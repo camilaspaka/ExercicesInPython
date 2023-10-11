bab3826eefcb302bb458c23adaf9b910aa954627
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-11 15:14:47 -0300

    Create getpass.py

---
+++ getpass.py
@@ -0,0 +1,7 @@
+usuario=input("Digite o usuário: ").upper()
+senha= input("Digite a senha: ")
+
+if usuario == "BITMED" and senha == "FiAp1222":
+    print("Usuário logado")
+else:
+    print("Login Negado")
