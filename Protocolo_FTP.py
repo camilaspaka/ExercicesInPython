d22fa99f9ddff26abc446e171d716fc8a3697975
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-15 20:54:55 -0300

    Update Protocolo_FTP.py

--- Protocolo_FTP.py
+++ Protocolo_FTP.py
@@ -2,4 +2,8 @@ from ftplib import *
 ftp_ativo=False
 ftp = FTP('ftp.ibiblio.org')
 print(ftp.getwelcome())
+usuario=input("Digite o usuario: ")
+senha=input("Digite a senha: ")
+ftp.login(usuario, senha)
+print("Diretório atual de trabalho: ", ftp.pwd())
 ftp.quit()
