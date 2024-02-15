60e5d7333b7bdf7edebf60aa99336630ae539209
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-02-15 15:18:13 -0300

    Update errorcode.py

--- errorcode.py
+++ errorcode.py
@@ -1,11 +1,10 @@
 hourlywage = input("Please enter your hourly wage: ")
 
-print("Hourly wage: ")
-print(hourlywage)
-hoursworked = input("Hours worked: ")
-print(hoursworked)
-
+print("Hourly wage: " + hourlywage)
 hours_worked = input("How many hours did you work this week? ")
+print("Hours worked: " + hours_worked)
+
+
 
 
 #thanks to the heavens
