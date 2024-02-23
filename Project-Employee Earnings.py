d285ffa3f00b8a47672b087ae33bbe135acbd753
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-02-23 13:56:30 -0300

    Update Project-Employee Earnings.py

--- Project-Employee Earnings.py
+++ Project-Employee Earnings.py
@@ -1,4 +1,4 @@
-name = input("Please enter the employee´s name: ")
+name = input("Please enter the employee´s name: ").strip().title()
 hourly_wage = input("Whats is their hourly wage? ")
 hours_worked = input("How many hours have they worked this week? ")
 
