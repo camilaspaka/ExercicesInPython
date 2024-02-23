6eb70110c2db3b2841ed8ac4b13fecfcd2228895
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-02-23 12:36:53 -0300

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,11 @@
+age = 29 #integer
+PI = 3.14159 #float
+
+maths_operations = 1 + 3 * 4 / 2 - 2
+print(maths_operations)
+
+float_division = 12 / 3
+print(float_division)
+
+integer_division = 12 // 3
+print(integer_division)
