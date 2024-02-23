6809e0c37743c5965b190f879910bed789cadc2f
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-02-23 13:58:52 -0300

    Update Project-Employee Earnings.py

--- Project-Employee Earnings.py
+++ Project-Employee Earnings.py
@@ -2,6 +2,5 @@ name = input("Please enter the employee´s name: ").strip().title()
 hourly_wage = input("Whats is their hourly wage? ")
 hours_worked = input("How many hours have they worked this week? ")
 
-print(name)
-print(hourly_wage)
-print(hours_worked)
+earnings = float(hourly_wage) * float(hours_worked)
+print(earnings)
