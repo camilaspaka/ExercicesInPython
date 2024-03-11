94a4f5bb27e78a478bb79639f76f168482dbf9c2
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-03-11 13:13:33 -0300

    Update integersbutdifferent.py

--- integersbutdifferent.py
+++ integersbutdifferent.py
@@ -1,2 +1,7 @@
 base_10 = 231
 print(f"This is the number in binary: {base_10 :b}")
+print(f"This is the number in hexadecimal: {base_10 :x}")
+print(f"This is the number in uppercase hexadecimal: {base_10 :X}")
+
+base_16 = int("E7" , base=16)
+print(f"This is the number in decimal: {base_16 :d}")
